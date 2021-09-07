c7ef4c5b9b7ba79f5e2ebaace609849bef406c62
xHossein <77378345+xHossein@users.noreply.github.com>
2021-09-07 17:58:48 +0430

    Bump 0.0.31

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setup(
     name='PyPasser',
-    version='0.0.3',
+    version='0.0.31',
     author='xHossein',
     license='MIT',
     url='https://github.com/xHossein/PyPasser',
